9a1e2983e16f4a25b8de4a4af3ba961accd44123
Mubasher <mobi_20_11@yahoo.com>
2023-09-26 14:48:52 +0200

    uppdated

--- section02_Project001/app.ts
+++ section02_Project001/app.ts
@@ -1,1 +1,16 @@
-console.log('testing the freaking code');
+// console.log('testing the freaking code');
+function add (n1:number,n2:number, showResult:boolean){
+  // console.log(typeof n1);
+  
+  return n1+n2;
+}
+
+const number1=5; //5.0 both numbers are having same value
+const number2=2.2;
+const printResult=true
+
+const result =add(number1,number2,printResult)
+console.log(result);
+
+
+

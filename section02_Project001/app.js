da057b6efdce34e5e4c9efde766a812d09c03ccf
Mubasher <mobi_20_11@yahoo.com>
2023-09-28 18:37:59 +0200

    unknown type

--- section02_Project001/app.js
+++ section02_Project001/app.js
@@ -1,10 +1,3 @@
-// console.log('testing the freaking code');
-function add(n1, n2, showResult) {
-    // console.log(typeof n1);
-    return n1 + n2;
-}
-var number1 = 5; //5.0 both numbers are having same value
-var number2 = 2.2;
-var printResult = true;
-var result = add(number1, number2, printResult);
-console.log(result);
+var userInput;
+userInput = 5;
+userInput = 'Max';

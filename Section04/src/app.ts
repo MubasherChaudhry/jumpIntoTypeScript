fb010edee8785f8af581ec8c37d764deeb24d69b
Mubasher <mobi_20_11@yahoo.com>
2023-10-02 14:40:17 +0200

    upated

--- Section04/src/app.ts
+++ Section04/src/app.ts
@@ -1,1 +1,36 @@
-// Code goes here!
+const userName='Mobi'
+
+//userName='MaxXx' we cant change const value as we let
+
+// let age=30;
+
+// age=35;
+//-----------Basic add Function----------------
+
+// function add (a: number, b: number){
+//   let result ;
+//   result =a+b;
+//   return result;
+
+// }
+
+// console.log(result);
+
+//-----------Arrow Function----------------
+
+// const add=(a:number,b:number)=>{
+//   return a+b;
+// }
+
+// console.log(add(9,5));
+
+//if you have only one expression then you can omit the curly braces and also can remove the return but the result of that on expression will be automaticly will return 
+//just a shorter way to use arrow function its valid only with on expression
+
+const add=(a:number,b:number)=>a+b;
+
+
+console.log(add(9,5));
+
+
+
